bc29c815c3fbb8fd643726f675726e74a5a537fd
amit03210 <ami9cse@gmail.com>
2026-01-26 20:57:31 +0530

    check

---
+++ 1. Python refresher/1. variables.py
@@ -0,0 +1,18 @@
+#Dyanmic typing: need not to declare variable type explicitly
+a = 13 #int
+b = 0.13 #float
+c = True #boolean
+x = "amit" #string
+numbers = [1,3,5] #lists
+person = {'name': 'amit', 'gender': 'male', 'mobile': 5291431} # dictionary
+
+print(type(x))
+
+#Naming rules:
+## Must start with letters or _.
+## Cannot start with numbers.
+## Case sensitive. name != Name
+## Avoid reserve words.
+
+
+
