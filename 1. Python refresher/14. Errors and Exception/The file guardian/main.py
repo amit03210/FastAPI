690bd9d2c71f803635303b47c9fae8e2a1f77575
amit03210 <102777713+amit03210@users.noreply.github.com>
2026-02-18 19:07:18 +0530

    Create The file guardian/main.py

---
+++ 1. Python refresher/14. Errors and Exception/The file guardian/main.py
@@ -0,0 +1,4 @@
+# EXERCISE 3: The File Guardian
+# Attempt to open a file named 'secrets.txt' in read mode.
+# Catch the FileNotFoundError and print a friendly message.
+# Use the 'finally' block to print "Execution Finished".
