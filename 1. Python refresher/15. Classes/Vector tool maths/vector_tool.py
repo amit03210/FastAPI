825684c296eca541f273fc52ac18ac2f5dea0767
amit03210 <ami9cse@gmail.com>
2026-03-02 07:33:37 +0530

    update

--- 1. Python refresher/15. Classes/Vector tool maths/vector_tool.py
+++ 1. Python refresher/15. Classes/Vector tool maths/vector_tool.py
@@ -1,6 +1,4 @@
 # FILE: vector_tool.py
-from dataclasses import dataclass
-
 """
 SCENARIO: Create a tool for 2D physics/graphics.
 INSTRUCTIONS:
@@ -10,3 +8,25 @@ INSTRUCTIONS:
 4. Implement '__repr__' so printing the vector looks like "Vector(x, y)".
 5. BONUS: Use '__slots__' in the dataclass to make it memory efficient.
 """
+from dataclasses import dataclass
+@dataclass(slots=True)
+class Vector:
+    '''Class for getting vectors'''
+    x: float
+    y: float
+
+    def __add__(self,other):
+        if self.__class__ is other.__class__:
+            return Vector(self.x+other.x, self.y+other.y)
+    
+    def __mul__(self, scalar):
+        return Vector(self.x*scalar, self.y*scalar)
+    
+
+vec1 = Vector(1,2)
+vec2 = Vector(2,3)
+
+print(vec1+vec2)
+print(vec2*4)
+print(vec2)
+
