135a6a0fc068f1186c3e5be5c65982bcac3f27e0
amit03210 <ami9cse@gmail.com>
2026-04-02 09:44:04 +0530

    update

--- JobNest/app/main.py
+++ JobNest/app/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI(title="JobNest", description="A scalable job board backend built with FastAPI", version="1.0.0")
+
+@app.get("/")
+def health_check():
+    """
+    Health check endpoint.
+    Real-world use: Load balancer ping this to know if the server is alive.
+    """
+    return {"status":"load",  "app" : "JobNest"}
