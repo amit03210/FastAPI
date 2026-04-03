97a6f9cb00f07c500742b17161648a58e8c911fc
amit03210 <ami9cse@gmail.com>
2026-04-03 22:22:14 +0530

    update

--- JobNest/app/main.py
+++ JobNest/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI(title="JobNest", description="A scalable job board backend built with FastAPI", version="1.0.0")
 
@@ -8,4 +9,4 @@ def health_check():
     Health check endpoint.
     Real-world use: Load balancer ping this to know if the server is alive.
     """
-    return {"status":"load",  "app" : "JobNest"}
+    return {"status":"load",  "app" : "JobNest", 'date': datetime.today().strftime('%H:%M:%S')}
